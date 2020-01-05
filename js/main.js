b00d51b34dbbbefe50d5d0c47e7da2b83c3d17ff
straus <straus94@gmail.com>
2020-01-05 23:04:44 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.js-slider').slick({
+    //   setting-name: setting-value
+        slidesToShow: 3
+        // arrows: false
+        // centerMode: true,
+        // centerPadding: '10px',
+    });
+});
